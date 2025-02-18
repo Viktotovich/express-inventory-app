694700ea8f71480b93413d710e59dc9126a0a890
Viktotovich <148013724+Viktotovich@users.noreply.github.com>
2025-02-18 16:21:00 +0400

    Fixed bug with form

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,6 @@
 const db = require("../db/queries");
 const links = require("../links");
+const { body, validationResult } = require("express-validator");
 
 module.exports.getCoursesPage = async function (req, res) {
   const title = "All courses you would ever need, right here - in one place!";
@@ -13,11 +14,11 @@ module.exports.getCreateCourse = async function (req, res) {
   const title = "Create your own course!";
   const courses = await db.getAllCourses();
 
-  res.render("pages/create-course", { title, links, courses });
+  res.render("pages/create-course", { title, links, courses: courses });
 };
 
 module.exports.postCreateCourse = async function (req, res) {
-  const { courseName, courseDescription } = req.params;
+  const { courseName, courseDescription } = req.body;
   await db.addCourse(courseName, courseDescription);
   res.redirect("/");
 };
